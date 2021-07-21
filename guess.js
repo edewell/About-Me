2890993aa2e8051d2483e48786755172650dcd2c
Eddie Ewell III <85755488+edewell@users.noreply.github.com>
2021-07-21 16:12:35 -0500

    Update guess.js

--- guess.js
+++ guess.js
@@ -1,1 +1,26 @@
+alert("Welcome to my page");
 
+let Askname = prompt("Can I get your name?"); 
+
+    console.log(askName); 
+
+    // give user a hint 
+
+ 
+ 
+ 
+
+let guessinggame = prompt("Are you ready to learn more about me?"); 
+
+{ 
+
+    let response = prompt("Are you ready to learn more about me?").toLowerCase 
+
+    if (response.toLowerCase() === 'y' || response.toLowerCase() === 'yes'); 
+
+    { 
+
+        guessinggame = false; 
+
+    } 
+} 
