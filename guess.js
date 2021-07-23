1e5791d31f5f277da34938f326e75b3f9d76fc9d
edewell <edewell3@gmail.com>
2021-07-22 20:23:27 -0500

    guess.html

--- guess.js
+++ guess.js
@@ -24,6 +24,9 @@ let response = prompt("Are you ready to learn more about me?")
     let response = prompt("Am I 19?") 
 
     if (response.toLowerCase() === 'n' || response.toLowerCase() === 'no');  
+    {
+        alert('great');
+    }
 } 
 
 let youSure = prompt("Are you sure?")
@@ -39,3 +42,31 @@ let car = prompt("Do I have a Nissan?")
 
     if (response.toLowerCase() === 'y' || response.toLowerCase() === 'yes');
 }
+
+let endGoal = prompt('Do I want to create an app?')
+{
+    let response = prompt('Do I want to create an app?')
+
+    if (response.toLowerCase() === 'y' || response.toLowerCase() === 'yes');
+}
+
+let number = prompt('Im thinking of a number can you guess it?')
+{
+    let response = prompt('"Im thinking of a number, can you guess it?')
+
+    if (response  > 5)
+    alert("Too high")
+
+    else if (reponse < 2)
+    alert("Too low");
+}
+
+let answer = prompt("How old are you?");
+console.log(answer);
+
+let age = ['19', '20', '21', '35', '40'];
+
+for (let i = 0; i < age.length; i++) {
+   console.log(age[i]); 
+}
+
